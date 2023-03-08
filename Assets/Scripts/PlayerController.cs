88c32989c2f3790aebca3d85238f99e6cab23f45
JadenCooper <jaden_cooper@outlook.com>
2023-03-08 23:02:49 +1300

    Added Basic Movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private InputActionReference Look, Movement;
+    [SerializeField]
+    private Vector3 MovementInput;
+    private Rigidbody rb;
+    public Vector3 movement;
+    public float MaxSpeed = 20;
+    public float CurrentSpeed = 0;
+    public float Acceleration = 10;
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    private Vector2 GetPointerPosition()
+    {
+        Vector3 mousePos = Look.action.ReadValue<Vector2>();
+        return Camera.main.ScreenToWorldPoint(mousePos);
+    }
+
+    public void Move()
+    {
+        CalculateSpeed();
+        MovementInput *= CurrentSpeed;
+        movement = MovementInput;
+    }
+
+    public void CalculateSpeed()
+    {
+        if (MovementInput == Vector3.zero)
+        {
+            CurrentSpeed += -Acceleration * Time.deltaTime;
+        }
+        else
+        {
+            CurrentSpeed += Acceleration * Time.deltaTime;
+        }
+        CurrentSpeed = Mathf.Clamp(CurrentSpeed, 0, MaxSpeed);
+    }
+
+    private void FixedUpdate()
+    {
+        MovementInput = Movement.action.ReadValue<Vector3>().normalized;
+        Move();
+        rb.velocity = movement * Time.deltaTime;
+    }
+}
