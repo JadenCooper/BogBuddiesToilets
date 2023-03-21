4a3ef7661da0f7a897d744b05403dc6cffd2fe82
Lamiaa115 <Lamialove115@gmail.com>
2023-03-22 04:34:16 +1300

    Comments

--- Assets/Scripts/ArrowMovement.cs
+++ Assets/Scripts/ArrowMovement.cs
@@ -8,21 +8,20 @@ using TMPro;
 
 public class ArrowMovement : MonoBehaviour
 {
+    //Location You are Moving to
     public Vector3 moveTo;
+    //Starting Text of the Arrow
     public string startingText;
+    //Gameobject refering where the text is.
     public TextMeshProUGUI text;
+    //rotation of character after teleportation.
     public float rotation;
+    //Location of arrows at the new location.
     public GameObject LocationArrows;
 
     // Start is called before the first frame update
     void Start()
     {
         text.text = startingText;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
--- Assets/Scripts/LookingMobile.cs
+++ Assets/Scripts/LookingMobile.cs
@@ -13,24 +13,39 @@ public class LookingMobile : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Grabs the Touch Input.
         Touch t = Input.GetTouch(0);
         if (t.phase == TouchPhase.Moved)
         {
+            //The location of where you are moving too.
             lookInput = t.deltaPosition * cameraSensitivity * Time.deltaTime;
+            //Makes it so you cannot look into yourself or right upwards.
             cameraPitch = Mathf.Clamp(cameraPitch - lookInput.y, -90f, 90f);
+            //Moves the camera slowly to the location of your finger movement.
+            
+            //for left/right
             cameraTransform.localRotation = Quaternion.Euler(cameraPitch, 0, 0);
+            
+            //for up/down
             transform.Rotate(transform.up, lookInput.x);
         }
         else if (t.phase == TouchPhase.Stationary)
         {
+            //If player is not moving their finger, no need to move the camera so its set to 0,0.
             lookInput = Vector2.zero;
         }
         else if (Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began)
         {
+            //Starts by sending out a raycast to check what object has been hit closest to the player.
             Ray raycast = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+
+            //raycasthit WILL become our gameobject that we interacted with.
             RaycastHit raycastHit;
+
+            //Checks IF you hit something and returns raycasthit WITH a gameobject attached to it.
             if (Physics.Raycast(raycast, out raycastHit))
             {
+                //Checks if the gameobjects tag is Arrow.
                 if (raycastHit.collider.CompareTag("Arrow"))
                 {
                     ArrowMovement objectHit = raycastHit.collider.gameObject.transform.GetComponent<ArrowMovement>();
@@ -38,9 +53,8 @@ public class LookingMobile : MonoBehaviour
                     objectHit.LocationArrows.SetActive(true);
                     objectHit.gameObject.transform.parent.gameObject.SetActive(false);
                 }
-            }
-            if (Physics.Raycast(raycast, out raycastHit))
-            {
+
+                //Checks if the game objects tag is Text
                 if (raycastHit.collider.CompareTag("Text"))
                 {
                     DisplayText textObj = raycastHit.collider.GetComponent<DisplayText>();
