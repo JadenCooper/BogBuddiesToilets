a272fbe63397851f82a8593edaff9c5fcd22cc17
JadenCooper <jaden_cooper@outlook.com>
2023-04-23 21:16:43 +1200

    Begun Work On Map

---
+++ Assets/Scripts/Waypoint.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoint : MonoBehaviour
+{
+    public ArrowManager arrowManager;
+    //Location You are Moving to
+    public Vector3 moveTo;
+    //rotation of character after teleportation.
+    public float rotation;
+    public  void Teleport()
+    {
+        arrowManager.MovePlayer(moveTo, rotation);
+    }
+}
