01a4463a634824d753837cdc1093713f61100de1
JadenCooper <JadenCooper@users.noreply.github.com>
2023-04-24 01:44:52 +0000

    Deploying to gh-pages from @ JadenCooper/BogBuddiesToilets@a03a13dd595823a013adb322c31442781677fcf2 🚀

--- Assets/Art/Models/ModelCredits.cs
+++
@@ -1,9 +0,0 @@
-/*
-
-"High Tank Toilet" (https://skfb.ly/oBHDD) 
-by Full Barrel Productions is licensed under Creative Commons Attribution (http://creativecommons.org/licenses/by/4.0/).
-
-"Urinal" (https://skfb.ly/6WRHv) 
-by ASA21 is licensed under Creative Commons Attribution (http://creativecommons.org/licenses/by/4.0/).
-
-*/
