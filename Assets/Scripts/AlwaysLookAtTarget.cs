df7b10226322f9d0ea7369b0be79c5abd4d5d279
Sharkules <kate.bull@hotmail.co.nz>
2023-04-27 09:40:08 +1200

    code review requested fixes - collab coding, Shaun + Kate

--- Assets/Scripts/AlwaysLookAtTarget.cs
+++ Assets/Scripts/AlwaysLookAtTarget.cs
@@ -4,11 +4,9 @@ using UnityEngine;
 
 public class AlwaysLookAtTarget : MonoBehaviour
 {
-    public Transform target;
-
     // Update is called once per frame
     void Update()
     {
-        transform.LookAt(target);    
+        transform.LookAt(Camera.main.gameObject.transform);    
     }
 }
