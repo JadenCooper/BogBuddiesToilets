5681cbea98afd0cca76ef624dcec051412e492af
JadenCooper <jaden_cooper@outlook.com>
2023-04-26 22:48:50 +1200

    Added Sound Options And Test Music To Prove Concept

--- Assets/Scripts/Manager/PauseManager.cs
+++ Assets/Scripts/Manager/PauseManager.cs
@@ -1,12 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PauseManager : MonoBehaviour
 {
     public GameObject PauseMenu;
     public LookingMobile look;
     public PlayerController playerController;
+    private Color orginalMapColor;
+    public Sprite map;
+    private Sprite orginalSprite;
+    private Image image;
+    private void Start()
+    {
+        image = PauseMenu.GetComponent<Image>();
+        orginalMapColor = image.color;
+        orginalSprite = image.sprite;
+    }
     public void Pause()
     {
         look.CanMove = !look.CanMove;
@@ -22,6 +33,20 @@ public class PauseManager : MonoBehaviour
             // Activate Pause Menu
             PauseMenu.SetActive(true);
             Time.timeScale = 0f;
+        }
+    }
+
+    public void MapChange()
+    {
+        if (image.color == orginalMapColor)
+        {
+            image.sprite = map;
+            image.color = Color.white;
+        }
+        else
+        {
+            image.sprite = orginalSprite;
+            image.color = orginalMapColor;
         }
     }
 }
