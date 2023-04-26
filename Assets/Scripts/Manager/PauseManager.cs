c248f8dbc0f7d27e6ed3f9e5f07188659c164695
JadenCooper <83618082+JadenCooper@users.noreply.github.com>
2023-04-27 11:32:49 +1200

    Made Menu Close On Teleport

--- Assets/Scripts/Manager/PauseManager.cs
+++ Assets/Scripts/Manager/PauseManager.cs
@@ -6,6 +6,9 @@ using UnityEngine.UI;
 public class PauseManager : MonoBehaviour
 {
     public GameObject PauseMenu;
+    public GameObject pauseButton;
+    public GameObject MapScreen;
+    public GameObject IntialScreen;
     public LookingMobile look;
     public PlayerController playerController;
     private Color orginalMapColor;
@@ -26,12 +29,14 @@ public class PauseManager : MonoBehaviour
         {
             // Pause Menu Is Already Active
             PauseMenu.SetActive(false);
+            pauseButton.SetActive(true);
             Time.timeScale = 1f;
         }
         else
         {
             // Activate Pause Menu
             PauseMenu.SetActive(true);
+            pauseButton.SetActive(false);
             Time.timeScale = 0f;
         }
     }
@@ -40,13 +45,19 @@ public class PauseManager : MonoBehaviour
     {
         if (image.color == orginalMapColor)
         {
+            Debug.Log("Map On");
             image.sprite = map;
             image.color = Color.white;
         }
         else
         {
+            Debug.Log("Map Off");
             image.sprite = orginalSprite;
             image.color = orginalMapColor;
+            IntialScreen.SetActive(true);
+            MapScreen.SetActive(false);
         }
     }
+
+
 }
