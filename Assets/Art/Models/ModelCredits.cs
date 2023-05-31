166f44c33c7e83a3b2e792a6c60a06a24876206a
JadenCooper <jaden_cooper@outlook.com>
2023-06-01 00:09:45 +1200

    Re Added Plant Models After Dealing With File Size Problems

--- Assets/Art/Models/ModelCredits.cs
+++ Assets/Art/Models/ModelCredits.cs
@@ -8,5 +8,6 @@ by ASA21 is licensed under Creative Commons Attribution (http://creativecommons.
 
 "Nelsons_Column"(https://www.turbosquid.com/3d-models/column-landmarks-max-free/190237)
 licensed = "https://blog.turbosquid.com/turbosquid-3d-model-license/"
+Converted Using "https://miconv.com/max-converter/"
 
 */
