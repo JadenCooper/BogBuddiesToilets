85a717370c6de0e02aca69a127a29faf3183cba3
JadenCooper <jaden_cooper@outlook.com>
2023-05-31 19:24:00 +1200

    Found Monument Stand In

--- Assets/Art/Models/ModelCredits.cs
+++ Assets/Art/Models/ModelCredits.cs
@@ -6,4 +6,7 @@ by Full Barrel Productions is licensed under Creative Commons Attribution (http:
 "Urinal" (https://skfb.ly/6WRHv) 
 by ASA21 is licensed under Creative Commons Attribution (http://creativecommons.org/licenses/by/4.0/).
 
+"Nelsons_Column"(https://www.turbosquid.com/3d-models/column-landmarks-max-free/190237)
+licensed = "https://blog.turbosquid.com/turbosquid-3d-model-license/"
+
 */
