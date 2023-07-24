c6bb1c575c1080b8a62109fd2f8f4b0934552d9c
mcfarlanejo <mcfarlanejo1086@gmail.com>
2023-07-24 14:22:17 +1200

    fixed movement. started working on fixing stairs

--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -5,18 +5,18 @@ using UnityEngine.InputSystem;
 
 public class MouseLook : MonoBehaviour
 {
-    public GameObject Player;
+    public GameObject camera;
     float xrotation = 0;
     float yrotation;
     Vector2 mousemovement;
     float camera_y;
-    Vector3 pos;
+    //Quaternion pos;
     public float mouse_sensitivity = 10f;
+    public bool mouseLocked = true;
 
     void Start()
     {
-        camera_y = transform.position.y - Player.transform.position.y;
-        Cursor.lockState = CursorLockMode.Locked;
+        mouseLocked = true;
     }
     
     
@@ -24,17 +24,24 @@ public class MouseLook : MonoBehaviour
     void Update()
     {
         //Update Mouse position to player position
-        pos = Player.transform.position;
-        pos.y += camera_y;
-        transform.position = pos;
+        //pos = Player.transform.rotation;
+        ////pos.y += camera_y;
+        //transform.rotation = pos;
         //Get mouse movement in new input system
-        mousemovement = Mouse.current.delta.ReadValue();     
-        xrotation -= mousemovement.y * Time.deltaTime * mouse_sensitivity;   
-        xrotation = Mathf.Clamp(xrotation,-90,90);
-        yrotation += mousemovement.x * Time.deltaTime * mouse_sensitivity;
+        if (!mouseLocked)
+        {
+            mousemovement = Mouse.current.delta.ReadValue();     
 
-        //Rotate the camera
-        transform.rotation = Quaternion.Euler(xrotation , yrotation , 0);
+            xrotation -= mousemovement.y * Time.deltaTime * mouse_sensitivity;   
+            xrotation = Mathf.Clamp(xrotation,-20,20);
+    
+            yrotation += mousemovement.x * Time.deltaTime * mouse_sensitivity;
+
+            //Rotate the camera
+            camera.transform.rotation = Quaternion.Euler(xrotation , yrotation , 0);
+            gameObject.transform.rotation = Quaternion.Euler(0 , yrotation , 0);
+        }
+       
     }
     
 }
