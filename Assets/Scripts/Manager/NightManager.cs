56ba35f816c30bc31235b888a696a3cd87050052
JadenCooper <jaden_cooper@outlook.com>
2023-07-26 13:41:40 +1200

    Made Lamps A Prefab For Merging

--- Assets/Scripts/Manager/NightManager.cs
+++ Assets/Scripts/Manager/NightManager.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class NightManager : MonoBehaviour
 {
     public GameObject Sun;
-    public List<GameObject> LampPostLights = new List<GameObject>();
+    public List<GameObject> Lights = new List<GameObject>();
 
     [ContextMenu("Toggle Night Day")] // Allows Developer To Activate The Method Through The Context Menu In The Inspector
     public void ToggleNightDay()
@@ -13,7 +13,7 @@ public class NightManager : MonoBehaviour
         // For Night Sun Will Be Turned Off And The Lights Turned On
         // For Day The Opposite
         Sun.SetActive(!Sun.activeSelf);
-        foreach (GameObject Light in LampPostLights)
+        foreach (GameObject Light in Lights)
         {
             Light.SetActive(!Light.activeSelf);
         }
