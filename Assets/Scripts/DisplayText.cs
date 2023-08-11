00354513ad6ffa87eebdf58d0d21bb906620741f
Shark <kate.bull@hotmail.co.nz>
2023-08-11 12:00:16 +1200

    aesthetic tweaks

--- Assets/Scripts/DisplayText.cs
+++ Assets/Scripts/DisplayText.cs
@@ -75,7 +75,6 @@ public class DisplayText : Interactable
                 //Set collectible as collected if exists
                 if(collectible)
                 {
-                    Debug.Log(collectible.title);
                     collectible.collected = true;
                 }
                 Seen = true;
