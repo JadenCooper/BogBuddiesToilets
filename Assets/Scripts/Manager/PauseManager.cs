347fcb629cedb042781979cad6e0d95b6c29fc27
JadenCooper <jaden_cooper@outlook.com>
2023-09-28 15:37:44 +1300

    Fixed Up Tidbit Counter

--- Assets/Scripts/Manager/PauseManager.cs
+++ Assets/Scripts/Manager/PauseManager.cs
@@ -17,14 +17,15 @@ public class PauseManager : MonoBehaviour
     private Image image;
     [SerializeField]
     private Vector2 textDisplayStatus = Vector2.zero; // X = Displays Seen Y = Total Displays
-    public List<DisplayText> DisplayTextList = new List<DisplayText>();
+    [SerializeField]
+    private List<Collectible> collectibles = new List<Collectible>();
     public TextMeshProUGUI textDisplayCount;
     public TextMeshProUGUI textDisplaySeenCount;
     public GameObject joysticks;
     private void Start()
     {
         image = PauseMenu.GetComponent<Image>();
-        textDisplayStatus.y = DisplayTextList.Count;
+        textDisplayStatus.y = collectibles.Count;
         orginalMapColor = image.color;
         orginalSprite = image.sprite;
     }
@@ -57,9 +58,9 @@ public class PauseManager : MonoBehaviour
     }
     public void UpdateTextDisplayTracker()
     {
-        for (int i = 0; i < DisplayTextList.Count; i++)
+        for (int i = 0; i < collectibles.Count; i++)
         {
-            if (DisplayTextList[i].Seen)
+            if (collectibles[i].collected)
             {
                 textDisplayStatus.x++;
             }
